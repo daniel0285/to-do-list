b591d822b1d0e4f3fc7b92e9ccd4301538c3094b
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2025-02-05 21:02:15 +0800

    feat: add class for task and array of projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+"strict mode";
 import "./styles.scss";
 
-console.log("test");
+const projects = [
+  {
+    projectTitle: "Default",
+    tasks: [
+      {
+        title: "Task #1",
+        description: "Description Here",
+        dueDate: "Today",
+        priority: "Important",
+        status: "Not Finished",
+      },
+    ],
+  },
+];
+
+class Task {
+  constructor(title, description, dueDate, priority, status) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+  }
+}
