d448cf1f93a8509c92aec604c4b8e532d7934492
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2025-02-10 21:20:17 +0800

    refactor: separate the file of DOM and TDL logic

--- src/DOM.js
+++ src/DOM.js
@@ -1,1 +1,86 @@
+import { projects } from "./index";
 
+const currentDisplay = document.querySelector("#currentDisplay > div");
+
+export function createTask(task, index, projectIndex) {
+  const taskDiv = document.createElement("div");
+  taskDiv.className = `task-item ${task.priority.toLowerCase()}`;
+  taskDiv.id = index;
+  taskDiv.dataset.projectIndex = projectIndex;
+
+  const status = document.createElement("p");
+  const title = document.createElement("h3");
+  const date = document.createElement("p");
+  const viewBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  status.textContent = task.status;
+  title.textContent = task.title;
+  date.textContent = task.date;
+  viewBtn.className = "btn view";
+  viewBtn.textContent = "View";
+  editBtn.className = "btn edit";
+  editBtn.textContent = "Edit";
+  deleteBtn.textContent = "Delete";
+
+  taskDiv.append(title, date, status, viewBtn, editBtn, deleteBtn);
+  return taskDiv;
+}
+
+function createTaskElements(tasks, index) {
+  const fragment = document.createDocumentFragment();
+  const projectIndex = index;
+
+  tasks.forEach((task, index) => {
+    fragment.append(createTask(task, index, projectIndex));
+  });
+
+  return fragment;
+}
+
+export function createProjectButton(title, index) {
+  const list = document.createElement("li");
+  const btn = document.createElement("button");
+
+  btn.innerText = title;
+  btn.dataset.projectIndex = index;
+  btn.className = "js-project-btn";
+  list.append(btn);
+
+  return list;
+}
+
+export function changeHeaderContent(text, index = 0) {
+  const projectTitle = document.querySelector(".project-title");
+  projectTitle.innerText = text;
+  projectTitle.dataset.projectIndex = index;
+}
+
+export function displayAllTasks() {
+  currentDisplay.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  changeHeaderContent("Home");
+
+  projects.forEach((project, index) => {
+    fragment.append(createTaskElements(project.tasks, index));
+  });
+
+  currentDisplay.append(fragment);
+}
+
+export function filterByProject(e) {
+  const text = e.target.textContent;
+  const projectIndex = e.target.dataset.projectIndex;
+  changeHeaderContent(text, projectIndex);
+
+  currentDisplay.innerHTML = "";
+  currentDisplay.append(
+    createTaskElements(projects[projectIndex].tasks, projectIndex)
+  );
+}
+
+export function toggleForm() {
+  const projectForm = document.getElementById("projectForm");
+  projectForm.classList.toggle("hidden");
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-"strict mode";
+"use strict";
 import "./styles.scss";
-const projects = [
+import * as TDLDom from "./DOM";
+
+export const projects = [
   {
     title: "Default",
     tasks: [
@@ -27,18 +29,6 @@ const projects = [
       },
     ],
   },
-  // {
-  //   title: "Default 2",
-  //   tasks: [
-  //     {
-  //       title: "Task #1 from default 2",
-  //       description: "Description Here",
-  //       date: "Today",
-  //       priority: "Important",
-  //       status: "Not Finished",
-  //     },
-  //   ],
-  // },
 ];
 
 class Task {
@@ -57,15 +47,15 @@ document.body.addEventListener("click", (e) => {
   }
 
   if (e.target.id === "home") {
-    displayAllTasks();
+    TDLDom.displayAllTasks();
   }
 
   if (e.target.id === "projectBtn") {
-    toggleForm();
+    TDLDom.toggleForm();
   }
 
   if (e.target.className === "js-project-btn") {
-    filterByProject(e);
+    TDLDom.filterByProject(e);
   }
 });
 
@@ -89,98 +79,12 @@ function createNewProject(e) {
   projects.push({ title: project.title, tasks: [] });
   const index = project.length - 1;
 
-  projectList.append(createProjectButton(project.title, index));
-  toggleForm();
+  projectList.append(TDLDom.createProjectButton(project.title, index));
+  TDLDom.toggleForm();
   e.target.reset();
 }
 
-function createProjectButton(title, index) {
-  const list = document.createElement("li");
-  const btn = document.createElement("button");
-
-  btn.innerText = title;
-  btn.dataset.projectIndex = index;
-  btn.className = "js-project-btn";
-  list.append(btn);
-
-  return list;
-}
-
-function createTaskElements(tasks, index) {
-  const fragment = document.createDocumentFragment();
-  const projectIndex = index;
-
-  tasks.forEach((task, index) => {
-    fragment.append(createTask(task, index, projectIndex));
-  });
-
-  return fragment;
-}
-
-function createTask(task, index, projectIndex) {
-  const taskDiv = document.createElement("div");
-  taskDiv.className = `task-item ${task.priority.toLowerCase()}`;
-  taskDiv.id = index;
-  taskDiv.dataset.projectIndex = projectIndex;
-
-  const status = document.createElement("p");
-  const title = document.createElement("h3");
-  const date = document.createElement("p");
-  const viewBtn = document.createElement("button");
-  const editBtn = document.createElement("button");
-  const deleteBtn = document.createElement("button");
-
-  status.textContent = task.status;
-  title.textContent = task.title;
-  date.textContent = task.date;
-  viewBtn.className = "btn view";
-  viewBtn.textContent = "View";
-  editBtn.className = "btn edit";
-  editBtn.textContent = "Edit";
-  deleteBtn.textContent = "Delete";
-
-  taskDiv.append(title, date, status, viewBtn, editBtn, deleteBtn);
-  return taskDiv;
-}
-
-const currentDisplay = document.querySelector("#currentDisplay > div");
-
-function filterByProject(e) {
-  const text = e.target.textContent;
-  const projectIndex = e.target.dataset.projectIndex;
-  changeHeaderContent(text, projectIndex);
-
-  currentDisplay.innerHTML = "";
-  currentDisplay.append(
-    createTaskElements(projects[projectIndex].tasks, projectIndex)
-  );
-}
-
-function changeHeaderContent(text, index = 0) {
-  const projectTitle = document.querySelector(".project-title");
-  projectTitle.innerText = text;
-  projectTitle.dataset.projectIndex = index;
-}
-
-function displayAllTasks() {
-  currentDisplay.innerHTML = "";
-  const fragment = document.createDocumentFragment();
-  changeHeaderContent("Home");
-
-  projects.forEach((project, index) => {
-    fragment.append(createTaskElements(project.tasks, index));
-  });
-
-  currentDisplay.append(fragment);
-}
-
-function toggleForm() {
-  const projectForm = document.getElementById("projectForm");
-  projectForm.classList.toggle("hidden");
-}
-
 function insertTask(e) {
-  const dialog = document.getElementById("dialog");
   const form = new FormData(e.target);
   const taskData = Object.fromEntries(form);
 
@@ -198,10 +102,8 @@ function insertTask(e) {
   );
 
   currentDisplay.append(
-    createTask(taskData, projects[index].tasks.length - 1, index)
+    TDLDom.createTask(taskData, projects[index].tasks.length - 1, index)
   );
   dialog.close();
   e.target.reset();
 }
-
-console.log(projects[0]);
